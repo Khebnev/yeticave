ec631a9449424623069a5a5584193c0877ba2c24
Anton <khebnev.a.v@gmail.com>
2021-11-28 18:51:15 +0200

    Заполнил ul.lots__list карточками товаров (двумерный массив), перебрал через foreach и добавил описание к карточкам на php

--- index.php
+++ index.php
@@ -133,24 +133,28 @@
             <h2>Открытые лоты</h2>
         </div>
         <ul class="lots__list">
-            <li class="lots__item lot">
-                <div class="lot__image">
-                    <img src="img/lot-1.jpg" width="350" height="260" alt="Сноуборд">
-                </div>
-                <div class="lot__info">
-                    <span class="lot__category">Доски и лыжи</span>
-                    <h3 class="lot__title"><a class="text-link" href="lot.html">2014 Rossignol District Snowboard</a></h3>
-                    <div class="lot__state">
-                        <div class="lot__rate">
-                            <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost">10 999<b class="rub">р</b></span>
-                        </div>
-                        <div class="lot__timer timer">
+        	<?php
+        	foreach ($categoryList as $key => $item): ?>
+	            <li class="lots__item lot">
+	                <div class="lot__image">
+	                    <img src="<?=$item['pic'];?>" width="350" height="260" alt="Сноуборд">
+	                </div>
+	                <div class="lot__info">
 
-                        </div>
-                    </div>
-                </div>
-            </li>
+	                    <span class="lot__category"><?=$item['category'];?></span>
+	                    <h3 class="lot__title"><a class="text-link" href="lot.html"><?=$item['name'];?></a></h3>
+	                    <div class="lot__state">
+	                        <div class="lot__rate">
+	                            <span class="lot__amount"><?=$item['price'];?></span>
+	                            <span class="lot__cost"><?=$item['price'];?><b class="rub">р</b></span>
+	                        </div>
+	                        <div class="lot__timer timer">
+
+	                        </div>
+	                    </div>
+	                </div>
+	            </li>
+        	<?php endforeach; ?>
         </ul>
     </section>
 </main>
